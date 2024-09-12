a88009315efb2a9bedb8e748c297e35107420873
dav74 <dav74130@gmail.com>
2024-09-12 10:26:20 +0200

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 api_key_llm = st.secrets["GROQ_API_KEY"]
 api_key_embedding = st.secrets["HF_API_KEY"]
-llm = Groq(temperature=0.8,model="llama3-70b-8192", api_key=api_key_llm)
+llm = Groq(temperature=0.8,model="llama-3.1-70b-versatile", api_key=api_key_llm)
 embed_model = HuggingFaceInferenceAPIEmbedding(model_name="OrdalieTech/Solon-embeddings-large-0.1", token = api_key_embedding)
 Settings.llm = llm
 Settings.embed_model = embed_model
@@ -14,7 +14,7 @@ storage_context = StorageContext.from_defaults(persist_dir='./index_storage')
 vector_index = load_index_from_storage(storage_context)
 st.set_page_config(page_title="ASSISTANT NSI (première et terminale)", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("Assistant NSI")
-st.text("version du 04/09/2024")
+st.text("version du 12/09/2024")
 st.info("Cet assistant vous permet de réviser le cours et faire des exercices.")
 if "messages" not in st.session_state.keys(): 
     st.session_state.messages = [
@@ -26,14 +26,13 @@ if "chat_engine" not in st.session_state.keys():
     system_prompt = (
         "Tu es un assistant spécialisé dans l'enseignement de la spécialité Numérique et sciences informatiques en classe de première et de terminal"
         'Tu as un bon niveau en langage Python'
-        'Tu dois commencer la conversation'
-        "Inspire-toi des sujets de bac donnés en exemple pour créer des exercices"
-        "Inspire-toi des sujets d'épreuve pratique pour créer des exercices sur la programmation en Python"
         'Ton interlocuteur est un élève qui suit la spécialité nsi en première et en terminale'
         'Tu dois uniquement répondre aux questions qui concernent la spécialité numérique et sciences informatiques'
         "Tu ne dois pas faire d'erreur, répond à la question uniquement si tu es sûr de ta réponse"
         "si tu ne trouves pas la réponse à une question, tu réponds que tu ne connais pas la réponse et que l'élève doit s'adresser à son professeur pour obtenir cette réponse"
         "Tu dois uniquement aborder des notions qui sont aux programmes de la spécialité numérique et sciences informatiques (première et terminale), tu ne dois jamais aborder une notion qui n'est pas au programme"
+        "si l'élève n'arrive pas à trouver la réponse à un exercice, tu ne dois pas lui donner tout de suite lui donner la réponse, mais seulement lui donner quelques indications"
+        "Yu peux lui donner, seulement si l'élève te demande explicitement la réponse"
         'Tu dois uniquement répondre en langue française'
         'les réponses doivent être données au format Markdown'
     )
